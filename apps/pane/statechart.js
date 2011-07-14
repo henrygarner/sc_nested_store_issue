01b68aca90e1d341f7a45388dba8c2b23e6e8e89
Maurits Lamers <maurits@weidestraat.nl>
2011-07-14 05:14:44 -0700

    Edited apps/pane/statechart.js via GitHub

--- apps/pane/statechart.js
+++ apps/pane/statechart.js
@@ -1,42 +1,64 @@
 /*globals Pane */
 
+Pane.yourView: SC.SheetPane.create({
+	layout: { width: 500, height: 200, centerX: 0, centerY: 0 },
+	defaultResponder: 'Pane.statechart',
+	contentView: SC.View.design({
+		childViews: 'labelView buttonView'.w(),
+		labelView: SC.ListView.design({
+			contentBinding: 'Pane.donglesController', 
+			contentValueKey: 'name',
+			layout: {top : 10, left: 10, width: 150, height: 180 }
+		}),
+		buttonView: SC.ButtonView.design({
+			target: 'parentView',
+			action: 'hidePane',
+			title: 'Cancel',
+			isCancel: YES,
+			layout: { top: 165, right: 15, width: 80 }
+		})
+	})
+})
+
+
 Pane.statechart = SC.Statechart.create({
-	initialState: 'mainState',
-	mainState: SC.State.extend({
+	initialState: 'MAINSTATE',
+	MAINSTATE: SC.State.design({
+
 		enterState: function() {
 			Pane.widgetController.set('content', Pane.store.find('Pane.Widget', 1));
 			Pane.mainPane = SC.TemplatePane.append({layerId: 'pane', templateName: 'pane'});
 		},
+		
 		showPane: function() {
-			this.set('store', Pane.store.chain());
-			var chainedWidget = this.get('store').find('Pane.Widget', 1);
-			Pane.donglesController.set('content', chainedWidget.get('dongles'));
-			
-			this.set('pane', SC.SheetPane.create({
-				layout: { width: 500, height: 200, centerX: 0, centerY: 0 },
-				contentView: SC.View.design({
-					childViews: 'labelView buttonView'.w(),
-					labelView: SC.ListView.design({
-						contentBinding: 'Pane.donglesController',
-						contentValueKey: 'name',
-						layout: {top : 10, left: 10, width: 150, height: 180 }
-					}),
-					buttonView: SC.ButtonView.design({
-						target: 'parentView',
-						action: 'hidePane',
-						title: 'Cancel',
-						isCancel: YES,
-						layout: { top: 165, right: 15, width: 80 }
-					}),
-					hidePane: function() {
-						Pane.statechart.sendEvent('hidePane');
-					}
-				})
-			}).append());
+      this.gotoState('SHOWINGPOPUP');
 		},
-		hidePane: function() {
-			this.get('store').discardChanges();
-			this.get('pane').remove();
-		}
+    
+    exitState: function(){
+    
+    }
+	}),
+	
+	SHOWINGPOPUP: SC.State.design({
+	  
+	  nestedStore: null,
+	  
+	  enterState: function(){
+	    this.nestedStore = Pane.store.chain();
+	    var chainedWidget = this.nestedStore.find('Pane.Widget', 1);
+			Pane.donglesController.set('content', chainedWidget.get('dongles'));
+			Pane.yourView.append();
+	  },
+	  
+	  hidePane: function(){
+	    this.gotoState('MAINSTATE');
+	  },
+	  
+	  exitState: function(){
+	    Pane.donglesController.set('content',null);
+	    this.nestedStore.destroy();
+	    this.nestedStore = null;
+			Pane.yourView.remove();
+	  }
 	})
 });
